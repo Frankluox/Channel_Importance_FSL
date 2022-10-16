dacfef80fdffb36ededdb0aee175945e84476c3d
Xu Luo <Frank.Luox@outlook.com>
2022-10-16 16:58:28 +0800

    Add oracle transformation

--- eval.sh
+++ eval.sh
@@ -1,6 +1,6 @@
-python test.py --gpu 0\
- --backbone_name resnet12\
+python test.py --backbone_name resnet12\
  --backbone_path /path/to/checkpoint\
  --dataset_name miniImageNet\
  --dataset_root /path/to/root\
- --classifier_name proto_head 
+ --classifier_name proto_head\ 
+ --use_Oracle True
