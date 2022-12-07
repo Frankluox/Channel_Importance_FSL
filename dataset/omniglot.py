aa50460f59ae4a16e32271fcfc14434541029a0c
Xu Luo <Frank.Luox@outlook.com>
2022-12-07 21:06:37 +0800

    Update omniglot.py

--- dataset/omniglot.py
+++ dataset/omniglot.py
@@ -8,8 +8,26 @@ class omniglot(Dataset):
         self.label = []
         for pair in self.dataset._flat_character_images:
             self.label.append(pair[1])
-    def __getitem__(self, index):
-        return self.dataset[index]
+
+    def __getitem__(self, index: int):
+        """
+        Args:
+            index (int): Index
+
+        Returns:
+            tuple: (image, target) where target is index of the target character class.
+        """
+        image_name, character_class = self.dataset._flat_character_images[index]
+        image_path = join(self.dataset.target_folder, self.dataset._characters[character_class], image_name)
+        image = Image.open(image_path, mode="r").convert('RGB')
+
+        if self.dataset.transform:
+            image = self.dataset.transform(image)
+
+        if self.dataset.target_transform:
+            character_class = self.dataset.target_transform(character_class)
+        return image, character_class
+
     def __len__(self):
         return len(self.dataset)
 
